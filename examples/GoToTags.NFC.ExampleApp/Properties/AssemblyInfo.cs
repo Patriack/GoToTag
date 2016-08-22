02df9a5ac9b3e8307b08698b03bd55574ac9abde
Craig Tadlock <craig@gototags.com>
2016-08-22 16:40:01 -0700

    initial project commit

---
+++ examples/GoToTags.NFC.ExampleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyCompany("GoToTags")]
+[assembly: AssemblyCopyright("GoToTags 2016")]
+[assembly: AssemblyTrademark("GoToTags")]
+[assembly: AssemblyDescription("http://gototags.com")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyTitle("GoToTags NFC Example App")]
+[assembly: AssemblyProduct("NFC Example App")]
