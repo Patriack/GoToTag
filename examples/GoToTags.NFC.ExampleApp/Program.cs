fddfb0b336517ace02953e9b99e66f13e89fd397
Craig Tadlock <craig@gototags.com>
2016-08-22 16:44:20 -0700

    initial nfc example app structure

--- examples/GoToTags.NFC.ExampleApp/Program.cs
+++ examples/GoToTags.NFC.ExampleApp/Program.cs
@@ -10,7 +10,31 @@ namespace GoToTags.NFC.ExampleApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.CancelKeyPress += Console_CancelKeyPress;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine();
+            }
+            finally
+            {
+                Dispose();
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Done. Press 'Enter' to quit.");
+            Console.ReadLine();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs args)
+        {
+        }
+
+        private static void Dispose()
+        {
         }
     }
 }
